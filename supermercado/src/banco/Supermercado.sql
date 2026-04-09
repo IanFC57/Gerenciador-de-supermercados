12e577bdc025e866a5a351a51a45aac1da6385ea
Ian Francisco <ianfchristani148@gmail.com>
2026-04-09 10:44:08 -0300

    Update Supermercado.sql

--- supermercado/src/banco/Supermercado.sql
+++ supermercado/src/banco/Supermercado.sql
@@ -5,12 +5,12 @@ create table usuario (
       id_usuario int primary key auto_increment,
       nome_usuario varchar(45) not null,
       cpf varchar(45) not null unique,
-      is_admin boolean not null -- NOVA COLUNA AQUI
+      is_admin boolean not null 
 );
 
 create table produto (
       id_produto int primary key auto_increment,
       nome_produto varchar(45) not null unique,
-      qtd int not null, -- Faltava essa vírgula no seu código
+      qtd int not null, 
       preco double not null
 );
